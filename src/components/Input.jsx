6c48cfe3ed260b47f8819314a66f52248081e2ae
Lenin Rojas Sánchez <leninrojassanchez09@gmail.com>
2025-07-28 23:29:06 -0600

    Pig latin translate function added

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,12 +5,38 @@ import Voice from './Voice'
 
 function Input({ setText }) {
 
+  function translatePigLatin(str) {
+    let newStr = '';
+    let consonantCluster = '';
+    let consonantFinished = false;
+    if(str[0].toUpperCase() == 'A' || str[0].toUpperCase() == 'E'||str[0].toUpperCase() == 'I'||str[0].toUpperCase() == 'O'||str[0].toUpperCase() == 'U') {
+      return str.concat('way');
+    } else {
+        for(let i = 0; i < str.length; i++) {
+          if(str[i].toUpperCase() != 'A' && str[i].toUpperCase() != 'E'&& str[i].toUpperCase() != 'I'&&str[i].toUpperCase() != 'O'&& str[i].toUpperCase() != 'U') {
+            if (!consonantFinished) {
+              consonantCluster += str[i];
+            } else {
+              newStr += str[i];
+            }
+          } else {
+            newStr += str[i];
+            consonantFinished = true;
+          }
+        }
+        newStr += consonantCluster; 
+        newStr += 'ay'
+        return newStr;
+    }
+}
   function updateText(e) {
     if (e.target.value === "") {
       setText("Translation");
       return;
+    } else {
+      setText(translatePigLatin(e.target.value));
     }
-    setText(e.target.value);
+    
   }
 
   return (
