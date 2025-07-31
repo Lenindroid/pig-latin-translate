3a60c048508eda360412cf4cf3cb7446d0741331
Lenin Rojas Sánchez <leninrojassanchez09@gmail.com>
2025-07-30 19:21:35 -0600

    Switch lang buttons

---
+++ src/components/SwitchLang.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRepeat } from '@fortawesome/free-solid-svg-icons'
+
+function SwitchLang() {
+  return (
+    <button>
+        <FontAwesomeIcon icon={faRepeat} />
+    </button>
+  )
+}
+
+export default SwitchLang
